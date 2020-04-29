bee814ba63373181cee7fd015feb2f15e8df9578
aleksei.ruzhinskiy <Alexrugik887>
2020-04-29 17:20:10 +0300

    test-githook#2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-jenkins-test';
+  title = 'angular-jenkins-test2';
 }
